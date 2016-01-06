e23786ad61215bca7ae68b3469a13e2f5315fb75
Roy Hewitt <royhewitt@Roys-Shiny-New-Macbook-Pro.local>
2016-01-05 22:36:14 -0500

    Add sample markers

--- app/js/map.js
+++ app/js/map.js
@@ -6,6 +6,8 @@
 
   var options, map;
 
+  L.Icon.Default.imagePath = '../images';
+
   var defaults = {
     zoom: 5,
     el: 'map'
@@ -15,6 +17,7 @@
     options = _.defaults({}, opts, defaults);
     createMap();
     addBasemap();
+    addMarkers();
   }
 
   function createMap() {
@@ -28,6 +31,11 @@
     L.tileLayer('http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', {
     	attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+  }
+
+  function addMarkers() {
+    var markers = L.geoJson(options.data).addTo(map);
+    map.fitBounds(markers.getBounds());
   }
 
   function handleEvents() {
